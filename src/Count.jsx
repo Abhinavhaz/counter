c21ba8965dc7b395d614d8b146d296e0ad5fb25b
Abhinav hazarika <abhinavhazarika27@gmail.com>
2024-05-10 00:33:19 +0530

    xyz

---
+++ src/Count.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Counter() {
+  const [valueOfCount, setValueOfCount] = useState(0);
+
+  const handleIncrement = () => {
+    setValueOfCount((prev) => prev + 1);
+  };
+
+  const handleDecrement = () => {
+    setValueOfCount((prev) => prev - 1);
+  };
+  return (
+    <div>
+      <h1>Counter App </h1>
+      <p>Count:{valueOfCount}</p>
+
+      <button name="Increment" onClick={handleIncrement}>
+        Increment{" "}
+      </button>
+      <button name="Decrement" onClick={handleDecrement}>
+        Decrement{" "}
+      </button>
+    </div>
+  );
+}
+export default Counter;
